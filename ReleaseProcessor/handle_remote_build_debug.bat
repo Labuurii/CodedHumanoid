9b1ff4f150eccd126417ea19042e31464cd0244c
Jerry <Labuurii@gmail.com>
2018-04-25 22:12:10 +0200

    handle_build bat now handles the new build_mode flag of ReleaseProcessor

--- ReleaseProcessor/handle_remote_build_debug.bat
+++ ReleaseProcessor/handle_remote_build_debug.bat
@@ -1,4 +1,4 @@
 cd "../CloudBuildDownloader/CloudBuildDownloader/bin/Debug"
 CloudBuildDownloader --build_mode=Debug --build_name="win64-debug" --platform="Windows" --project_name="pl-war"
 cd ../../../../ReleaseProcessor
-dmd -debug -g -run ReleaseProcessor.d --stage=PostProcess
+dmd -debug -g -run ReleaseProcessor.d --stage=PostProcess --build_mode=Debug
